64cfb88b3c48d19b1776f94f547c5dcc608935fd
Lily2 <168254582+Lily29371@users.noreply.github.com>
2025-10-18 20:35:54 +0700

    Update Sphere.java

--- src/main/java/me/catmi/util/render/Sphere.java
+++ src/main/java/me/catmi/util/render/Sphere.java
@@ -1,1 +1,10 @@
+package me.catmi.util.render;
 
+// Placeholder Sphere thay cho javafx.scene.shape.Sphere
+public class Sphere {
+    public float radius;
+
+    public Sphere(float radius) {
+        this.radius = radius;
+    }
+}
